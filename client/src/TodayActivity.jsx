b3188bc10d0b9674665309fdbc3a97fd8da1c8bf
betttyquu <absheng947@hotmail.com>
2019-06-24 20:24:19 +0000

    added toggle effect to calendar icon

--- client/src/TodayActivity.jsx
+++ client/src/TodayActivity.jsx
@@ -2,11 +2,22 @@ import React, {Component} from 'react';
 import TodayActivityBox from './TodayActivityBox.jsx';
 import TodayActivityCalender from './TodayActivityCalender.jsx';
 import { Icon } from 'antd';
+import { Calendar } from 'antd';
 
 class TodayActivity extends Component {
-    handleClick = () => {
-        console.log('this is', this);
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: true,
+        };
     }
+
+    handleClick = () => {
+        this.setState({
+            active: !this.state.active
+        });
+    }
+
     render() {
         console.log('this is the props', this.props.activities)
         // const activity = this.props.activities
@@ -16,6 +27,7 @@ class TodayActivity extends Component {
             <section className="Actvity">
                 <div className="todayActivityIcon">
                     <Icon type="schedule" onClick={this.handleClick}/>
+                    {this.state.active && <Calendar fullscreen={false} className="dayActivities_calendar"/>}
                 </div>
                 <div className="TodayActivityBox">
                     <h2>Activity</h2>
