d6445a99f11c7a86c9eb7a9e950dfd4aa8071de7
Jerry Chuang <jerry@Jerrys-MacBook-Air.local>
2019-06-24 14:20:03 -0700

    Move day activities code to appropriate controller

--- app/controllers/api/user_activities_controller.rb
+++ app/controllers/api/user_activities_controller.rb
@@ -1,2 +1,13 @@
 class Api::UserActivitiesController < ApplicationController
+  def index
+    @user_agenda = User.find_by(email:params['email']).user_agendas.last
+    @user_activities = @user_agenda.user_activities.where(date: params['date'])
+    @activities = @user_activities.map {|user_activity|user_activity.activity}
+    @categories = @activities.map {|activity|activity.category.name}.uniq
+
+    render :json => {
+      activities: @activities,
+      categories: @categories
+    }
+  end
 end
--- app/controllers/api/user_agendas_controller.rb
+++ app/controllers/api/user_agendas_controller.rb
@@ -1,13 +1,2 @@
 class Api::UserAgendasController < ApplicationController
-  def index
-    @user_agenda = User.find_by(email:params['email']).user_agendas.last
-    @user_activities = @user_agenda.user_activities.where(date: params['date'])
-    @activities = @user_activities.map {|user_activity|user_activity.activity}
-    @categories = @activities.map {|activity|activity.category.name}.uniq
-
-    render :json => {
-      activities: @activities,
-      categories: @categories
-    }
-  end
 end
