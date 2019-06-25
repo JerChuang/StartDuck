cb1938a28d3699be0bf52dfbee7eafb7a169f48b
Jerry Chuang <jerry@Jerrys-MacBook-Air.local>
2019-06-25 14:33:04 -0700

    Add main page and logo image as .svg

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './images/logo.png';
+import { ReactComponent as Logo } from './images/logo.svg';
 import {Link} from "react-router-dom";
-console.log(logo);
+
 const Nav = (props) =>(
   <nav className="navbar">
    <div className="container-fluid">
-    <img className="navbar_logo" src={logo} alt = "logo"/>
+
+    <Logo className="navbar_logo" />
 
      <Link className="link" to="/schedule" currentpath = '/'>Reschedule</Link>
      <Link className="link" to="/completed_activities" currentpath = '/'>Completed Activities</Link>
