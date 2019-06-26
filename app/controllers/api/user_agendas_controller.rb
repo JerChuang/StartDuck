41eb42c6a66ddefa6f68fbf7a7496cf60d2f87bd
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-06-26 23:05:34 +0000

    receive data from ruby

--- app/controllers/api/user_agendas_controller.rb
+++ app/controllers/api/user_agendas_controller.rb
@@ -1,4 +1,20 @@
 class Api::UserAgendasController < ApplicationController
   def create
+    @agenda = UserAgenda.new(agenda_params)
+    @user = User.find_by(email:params['email'])
+    @user_id = @user.id
+    @agenda.user_id = @user_id
+    @agenda.save
+
   end
+
+private
+  def agenda_params
+    params.require(:user_agenda).permit(
+        :start_date,
+        :end_date,
+        :hours_per_day
+    )
+  end
+
 end
