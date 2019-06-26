eb888d82cdc166b0ac92380f4d39f1a2ca632ec6
betttyquu <absheng947@hotmail.com>
2019-06-26 12:38:51 -0700

    adds code to render activity 1

--- app/controllers/api/user_activities_controller.rb
+++ app/controllers/api/user_activities_controller.rb
@@ -12,15 +12,20 @@ class Api::UserActivitiesController < ApplicationController
       categories: @categories
     }
   end
-  
+
   def show
     @user_agenda = User.find_by(email:params['email']).user_agendas.last
     @user_activities = @user_agenda.user_activities.where(date: params['date'])
-    @activities = @user_activities.map{|user_activity|user_activity.activity}
+    
+    @activities = @user_activities.joins("INNER JOIN activities ON user_activities.activity_id = activities.id")
+    # @activities = @user_activities.map{|user_activity|user_activity.activity}
     @categories = @user_activities.map {|user_activity|user_activity.activity.category.name}.uniq
+    byebug
+    @activity = @activities.where(activity: '1') 
     render :json => {
       activities: @activities,
-      categories: @categories
+      categories: @categories,
+      activity: @activity
     }
   end
 
--- client/src/TodayActivityBox.jsx
+++ client/src/TodayActivityBox.jsx
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 
 const TodayActivityBox = (props) => {
+    // console.log('this is from box', this.state)
+    // console.log('this is from box', this.props)
 
     return (
+
         <div className="todayActivityBox">
             <h1 className="todayActivityTitle">{props.activities[0].name}</h1>
             <span className="todayActivityDuration">{props.activities[0].duration} m</span>
