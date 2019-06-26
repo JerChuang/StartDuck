cb62138be936976f58f94b0946d8479d521e144b
Jerry Chuang <jerry@Jerrys-MacBook-Air.local>
2019-06-25 20:41:07 -0700

    Add controller functions for create/destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+      session[:user_id] = user.id
+      redirect_to '/'
+  end
+
+  def destroy
+    session[:user_id] = nil
+  end
 end
