ec307c5d28e5736dabb37dc4a7b4b5a920f8eab0
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-06-27 02:25:17 +0000

    add ruby side code to receive data from react side

--- app/controllers/api/user_agendas_controller.rb
+++ app/controllers/api/user_agendas_controller.rb
@@ -1,11 +1,21 @@
 class Api::UserAgendasController < ApplicationController
   def create
+
     @agenda = UserAgenda.new(agenda_params)
     @user = User.find_by(email:params['email'])
     @user_id = @user.id
     @agenda.user_id = @user_id
     @agenda.save
 
+    @category_id = Category.where(name: params[:categories]).pluck(:id)
+    @activities = Activity.where(category_id: @category_id)
+
+    @activities.each do |activity|
+      UserActivity.create!(
+        activity: activity,
+        user_agenda: @agenda
+        )
+    end
   end
 
 private
