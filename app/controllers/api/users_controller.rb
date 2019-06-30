f5fda5bf886fa41fd4783dbc49b1814241152fa6
Jerry Chuang <jerry@Jerrys-MacBook-Air.local>
2019-06-30 10:55:58 -0700

    Allow create new user by entering new email

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,8 @@
 class Api::UsersController < ApplicationController
+  def create
+    user = User.create(:email => params['email'])
+  end
+
   def show
     @completed_activities = User.find_by(email:params['email']).user_activities.where(completeness: true)
     @activities = @completed_activities.select('user_activities.*, activities.*').joins("INNER JOIN activities ON user_activities.activity_id = activities.id")
